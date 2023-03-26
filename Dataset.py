a989979cf54ddc2a490ef9fbd7df95f57dd33018
HalmosiL <halmosilevente@gmail.com>
2023-03-26 15:47:14 +0200

    init

---
+++ Dataset.py
@@ -0,0 +1,45 @@
+import torchvision.transforms as transforms
+import torchvision
+import torch
+
+transform_train =  transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+])
+
+transform_test =  transforms.Compose([
+    transforms.ToTensor(),
+])
+
+def getTrainset(BATCH_SIZE_TRAIN, NOM_WORKERS_TRAIN, PIN_MEMORY=False):
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data',
+        train=True,
+        download=True,
+        transform=transform_train
+    )
+
+    return torch.utils.data.DataLoader(
+        trainset,
+        batch_size=BATCH_SIZE_TRAIN,
+        shuffle=True,
+        num_workers=NOM_WORKERS_TRAIN,
+        pin_memory=PIN_MEMORY
+    )
+
+def getTestset(BATCH_SIZE_TEST, NOM_WORKERS_TEST, PIN_MEMORY=False):
+    testset = torchvision.datasets.CIFAR10(
+        root='./data',
+        train=False,
+        download=True,
+        transform=transform_test
+    )
+
+    return torch.utils.data.DataLoader(
+        testset,
+        batch_size=BATCH_SIZE_TEST,
+        shuffle=False,
+        num_workers=NOM_WORKERS_TEST,
+        pin_memory=PIN_MEMORY
+    )
---
+++ Model.py
@@ -0,0 +1,12 @@
+from Network import ResNet18
+import torch
+
+def getModel():
+    return ResNet18()
+
+def loadModel(PATH):
+    model = ResNet18()
+    model.load_state_dict(torch.load(PATH))
+    model.eval()
+
+    return model
